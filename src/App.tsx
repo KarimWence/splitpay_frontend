ac4ee56393035cd2f562f9356fb84f9500b8427e
Karim Dasaev Wence Landin <kwence_22@alu.uabcs.mx>
2026-06-02 19:15:16 -0700

    feat: add date-fns dependency and update App component to include SocketProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import { AppRouter } from './app/router'
 
 import { useSync } from '@/features/sync/hooks/use-sync'
+import { useNetworkToasts } from './features/sync/hooks/use-network-toasts'
+import { SocketProvider } from './features/socket/socket-provider'
 
 function App() {
     useSync()
+    useNetworkToasts()
 
-    return <AppRouter />
+    return (
+        <SocketProvider>
+            <AppRouter />
+        </SocketProvider>
+    )
 }
 
 export default App
